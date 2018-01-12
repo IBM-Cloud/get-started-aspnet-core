0688e50857a7555491ddc6ec62d96b138adf181a
NTripleA <nelson.alemar@upr.edu>
2018-01-12 11:38:08 -0500

    added services folder with two files and two more model files: creds and todoitems

---
+++ src/GetStartedDotnet/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace GetStartedDotnet.Models
+{
+    public class ToDoItem
+    {
+        public string id { get; set; }
+        public string rev { get; set; }
+        public string text { get; set; }
+
+    }
+}
